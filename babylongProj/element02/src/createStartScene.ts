258d40399aa27a04dfc564462b5e9f7189fdeb06
ReissHamilton <145940814+ReissHamilton@users.noreply.github.com>
2023-11-23 11:14:55 +0000

    week 12 changes

--- babylongProj/element02/src/createStartScene.ts
+++ babylongProj/element02/src/createStartScene.ts
@@ -119,6 +119,34 @@ import {
     const box = createBox(scene);
     const roof = createRoof(scene);
     const house: any = Mesh.MergeMeshes([box, roof], true, false, undefined, false, true);
+
+  //   const places = []; //each entry is an array [house type, rotation, x, z]
+  //   places.push([1, -Math.PI / 16, -6.8, 2.5 ]);
+  //   places.push([2, -Math.PI / 16, -4.5, 3 ]);
+  //   places.push([2, -Math.PI / 16, -1.5, 4 ]);
+  //   places.push([2, -Math.PI / 3, 1.5, 6 ]);
+  //   places.push([2, 15 * Math.PI / 16, -6.4, -1.5 ]);
+  //   places.push([1, 15 * Math.PI / 16, -4.1, -1 ]);
+  //   places.push([2, 15 * Math.PI / 16, -2.1, -0.5 ]);
+  //   places.push([1, 5 * Math.PI / 4, 0, -1 ]);
+  //   places.push([1, Math.PI + Math.PI / 2.5, 0.5, -3 ]);
+  //   places.push([2, Math.PI + Math.PI / 2.1, 0.75, -5 ]);
+  //   places.push([1, Math.PI + Math.PI / 2.25, 0.75, -7 ]);
+  //   places.push([2, Math.PI / 1.9, 4.75, -1 ]);
+  //   places.push([1, Math.PI / 1.95, 4.5, -3 ]);
+  //   places.push([2, Math.PI / 1.9, 4.75, -5 ]);
+  //   places.push([1, Math.PI / 1.9, 4.75, -7 ]);
+  //   places.push([2, -Math.PI / 3, 5.25, 2 ]);
+  //   places.push([1, -Math.PI / 3, 6, 4 ]);
+
+  //   for (let i = 0; i < places.length; i++) {
+  //     if (places[i][0] === 1) {
+  //         house[i].createInstance("house" + i);
+  //     }
+  //     house[i].rotation.y = places[i][1];
+  //     house[i].position.x = places[i][2];
+  //     house[i].position.z = places[i][3];
+  // }
     return house;
   }
 
---
+++ babylongProj/element04/src/MenuScene.ts
@@ -0,0 +1,133 @@
+//-----------------------------------------------------------
+// TOP OF CODE - IMPORTING BABYLONS
+import setSceneIndex from "./index";
+import "@babylonjs/core/Debug/debugLayer";
+import "@babylonjs/inspector";
+import {
+    Scene,
+    ArcRotateCamera,
+    Vector3,
+    HemisphericLight,
+    MeshBuilder,
+    Mesh,
+    Light,
+    Camera,
+    Engine,
+    Texture,
+    StandardMaterial,
+    CubeTexture,
+    Color3,
+    SceneLoader,
+    ActionManager,
+    ExecuteCodeAction,
+    AnimationPropertiesOverride,
+    Sound,
+  } from "@babylonjs/core";
+  import * as GUI from "@babylonjs/gui";
+  import HavokPhysics from "@babylonjs/havok";
+  import { HavokPlugin, PhysicsAggregate, PhysicsShapeType } from "@babylonjs/core";
+  //---------------------------------------------------------
+  
+  function createSceneButton(scene: Scene, name: string, index: string, x: string, y: string, advtex) {
+    let button = GUI.Button.CreateSimpleButton(name, index);
+        button.left = x;
+        button.top = y;
+        button.width = "160px";
+        button.height = "60px";
+        button.color = "white";
+        button.cornerRadius = 20;
+        button.background = "purple";
+
+        const buttonClick = new Sound("MenuClickSFX", "./Audio/menu-click.wav", scene, null, {
+          loop: false,
+          autoplay: false,
+        });
+
+        button.onPointerUpObservable.add(function() {
+          buttonClick.play();
+          setSceneIndex(1);
+        });
+        advtex.addControl(button);
+        return button;
+    }
+ 
+  //---------------------------------------------------------
+  //---------------------------------------------------------
+
+  // // MIDDLE OF CODE - FUNCTIONS
+
+  // Create Skybox
+  function createSkybox(scene: Scene) {
+    //Skybox
+    const skybox = MeshBuilder.CreateBox("skyBox", {size:150}, scene);
+	  const skyboxMaterial = new StandardMaterial("skyBox", scene);
+	  skyboxMaterial.backFaceCulling = false;
+	  skyboxMaterial.reflectionTexture = new CubeTexture("textures/skybox", scene);
+	  skyboxMaterial.reflectionTexture.coordinatesMode = Texture.SKYBOX_MODE;
+	  skyboxMaterial.diffuseColor = new Color3(0, 0, 0);
+	  skyboxMaterial.specularColor = new Color3(0, 0, 0);
+	  skybox.material = skyboxMaterial;
+    return skybox;
+  }
+
+  
+  function createLight(scene: Scene) {
+    const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+    light.intensity = 0.7;
+    return light;
+  }
+  
+  function createArcRotateCamera(scene: Scene) {
+    let camAlpha = -Math.PI / 2,
+      camBeta = Math.PI / 2.5,
+      camDist = 10,
+      camTarget = new Vector3(0, 0, 0);
+    let camera = new ArcRotateCamera(
+      "camera1",
+      camAlpha,
+      camBeta,
+      camDist,
+      camTarget,
+      scene,
+    );
+    camera.attachControl(true);
+    return camera;
+  }
+  //-------------------------------------------------------
+  //-------------------------------------------------------
+  // BOTTOM OF CODE - MAIN RENDERING
+  
+  export default function MenuScene(engine: Engine) {
+    interface SceneData {
+      scene: Scene;
+      skybox?: Mesh;
+      //box?: Mesh;
+      light?: Light;
+      sphere?: Mesh;
+      //ground?: Mesh;
+      //importMesh?: any;
+      //actionManager?: any;
+      camera?: Camera;
+    }
+  
+    let that: SceneData = { scene: new Scene(engine) };
+    that.scene.debugLayer.show();
+    // Initialise Physics
+    //that.scene.enablePhysics(new Vector3(0, -9.8, 0), havokPlugin);
+    //-------------------------------------------------------
+
+    let advancedTexture = GUI.AdvancedDynamicTexture.CreateFullscreenUI("myUI", true);
+    let button1 = createSceneButton(that.scene, "but1", "Start Game", "0px", "-75px", advancedTexture);
+
+    // Spawn Assets
+    that.skybox = createSkybox(that.scene);
+    that.light = createLight(that.scene);
+    //that.box = createBox(that.scene, 2, 2, 2);
+    //that.ground = createGround(that.scene);
+    //that.importMesh = importPlayerMesh(that.scene, that.box, 0 , 0);
+    //that.actionManager = actionManager(that.scene);
+    that.camera = createArcRotateCamera(that.scene);
+
+    // that.house = createHouse(that.scene)
+    return that;
+  }
