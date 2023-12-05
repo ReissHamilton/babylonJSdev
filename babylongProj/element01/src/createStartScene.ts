f6ae183a3b155c5bd42afc5107322c067f7602e6
ReissHamilton <145940814+ReissHamilton@users.noreply.github.com>
2023-12-05 17:41:05 +0000

    element 1 & 3 done

--- babylongProj/element01/src/createStartScene.ts
+++ babylongProj/element01/src/createStartScene.ts
@@ -1,31 +1,28 @@
-//-----------------------------------------------------
-//TOP OF CODE - IMPORTING BABYLONJS
+//-------------------------------------------------------
+// TOP OF CODE - IMPORTING BABYLONS
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
 import {
-    Scene,
-    ArcRotateCamera,
-    Vector3,
-    Vector4,
-    HemisphericLight,
-    SpotLight,
+    Scene, ArcRotateCamera, Vector3, HemisphericLight,
     MeshBuilder,
     Mesh,
-    Light,
+    Light, Color3,
     Camera,
     Engine,
-    StandardMaterial,
-    Texture,
-    Color3,
     Space,
+    SpotLight,
     ShadowGenerator,
     PointLight,
-    DirectionalLight,
+    StandardMaterial,
+    Vector4,
+    Texture
   } from "@babylonjs/core";
-  //----------------------------------------------------
-  
-  //----------------------------------------------------
-  //MIDDLE OF CODE - FUNCTIONS
+  //-------------------------------------------------------
+
+  //-------------------------------------------------------
+  // MIDDLE OF CODE - FUNCTIONS
+
+  //faced box function
   function createBox(scene: Scene, px: number, py: number, pz: number, sx: number, sy: number, sz: number, rotation: boolean) {
     let box = MeshBuilder.CreateBox("box",{size: 1}, scene);
     box.position = new Vector3(px, py, pz);
@@ -42,7 +39,6 @@ import {
     return box;
   }
 
-  //faced box function
   function createFacedBox(scene: Scene, px: number, py: number, pz: number) {
     const mat = new StandardMaterial("mat");
     const texture = new Texture("https://assets.babylonjs.com/environments/numbers.jpg");
@@ -72,25 +68,7 @@ import {
     return box;
   }
 
-  function createTorus(scene: Scene, px: number, py: number, pz: number) {
-    const torus = MeshBuilder.CreateTorus("torus", {});
-    torus.position = new Vector3(px, py, pz);
-    scene.registerAfterRender(function () {
-      torus.rotate(new Vector3(4, 8, 2)/*axis*/, 0.02/*angle*/, Space.LOCAL);
-    });
-    return torus;
-  }
-
-  //Type refers to Polyhedra guide: https://doc.babylonjs.com/features/featuresDeepDive/mesh/creation/polyhedra/polyhedra_by_numbers
-  function createPolyhedra(scene: Scene, t: number, s: number, px: number, py: number, pz: number) {
-    const polyhedra = MeshBuilder.CreatePolyhedron("shape", {type: t, size: s}, scene);
-    polyhedra.position = new Vector3(px, py, pz);
-    scene.registerAfterRender(function () {
-      polyhedra.rotate(new Vector3(4, 8, 2)/*axis*/, 0.02/*angle*/, Space.LOCAL);
-    });
-    return polyhedra;
-  }
-
+  
   function createAnyLight(scene: Scene, index: number, px: number, py: number, pz: number, colX: number, colY: number, colZ: number, mesh: Mesh) {
     // only spotlight, point and directional can cast shadows in BabylonJS
     switch (index) {
@@ -123,25 +101,41 @@ import {
     light.intensity = 0.3;
     return light;
   }
-
-  function createSphere(scene: Scene) {
-    let sphere = MeshBuilder.CreateSphere(
-      "sphere",
-      { diameter: 2, segments: 32 },
-      scene,
-    );
-    sphere.position.y = 1;
+  
+  function createSphere(scene: Scene, px: number, py: number, pz: number) {
+    const sphere = MeshBuilder.CreateSphere("sphere", {});
+    sphere.position = new Vector3(px, py, pz);
+    scene.registerAfterRender(function () {
+      sphere.rotate(new Vector3(4, 8, 2)/*axis*/, 0.02/*angle*/, Space.LOCAL);
+    });
     return sphere;
   }
+
+  function createTorus(scene: Scene, px: number, py: number, pz: number) {
+    const torus = MeshBuilder.CreateTorus("torus", {});
+    torus.position = new Vector3(px, py, pz);
+    scene.registerAfterRender(function () {
+      torus.rotate(new Vector3(4, 8, 2)/*axis*/, 0.02/*angle*/, Space.LOCAL);
+    });
+    return torus;
+  }
+
+  //Type refers to Polyhedra guide: https://doc.babylonjs.com/features/featuresDeepDive/mesh/creation/polyhedra/polyhedra_by_numbers
+  function createPolyhedra(scene: Scene, t: number, s: number, px: number, py: number, pz: number) {
+    const polyhedra = MeshBuilder.CreatePolyhedron("shape", {type: t, size: s}, scene);
+    polyhedra.position = new Vector3(px, py, pz);
+    scene.registerAfterRender(function () {
+      polyhedra.rotate(new Vector3(4, 8, 2)/*axis*/, 0.02/*angle*/, Space.LOCAL);
+    });
+    return polyhedra;
+  }
   
-  function createGround(scene: Scene, w: number, h: number, px: number) {
+  function createGround(scene: Scene) {
     let ground = MeshBuilder.CreateGround(
       "ground",
-      { width: w, height: h },
+      { width: 6, height: 6 },
       scene,
     );
-    ground.receiveShadows = true;
-    ground.position.x = px;
     return ground;
   }
   
@@ -161,10 +155,10 @@ import {
     camera.attachControl(true);
     return camera;
   }
-  //----------------------------------------------------------
-  
-  //----------------------------------------------------------
-  //BOTTOM OF CODE - MAIN RENDERING AREA FOR YOUR SCENE
+  //-------------------------------------------------------
+
+  //-------------------------------------------------------
+  // BOTTOM OF CODE - MAIN RENDERING
   export default function createStartScene(engine: Engine) {
     interface SceneData {
       scene: Scene;
@@ -182,27 +176,28 @@ import {
     let that: SceneData = { scene: new Scene(engine) };
     that.scene.debugLayer.show();
   
-    //Scene Lighting
     that.hemisphericLight = createHemiLight(that.scene);
-    //shape 1 and light
+
+    //shape 1 and 
     that.faceBox = createFacedBox(that.scene, -10, 2, 0);
     that.light = createAnyLight(that.scene, 2, -10, 5, 0, 0.75, 0.12, 0.91, that.faceBox);
 
-    //shape 2 and light
+    //shape 2 and
     that.box = createBox(that.scene, -5, 2, 0, 3, 2, 1, true);
+    that.light = createAnyLight(that.scene, -5, 2, 5, 0, 0.12, 0.64, 0.86, that.box);
 
-    //shape 3 and light
+    // Shape 3
     that.torus = createTorus(that.scene, 0, 2, 0);
     that.light = createAnyLight(that.scene, 2, 0, 5, 0, 0.12, 0.64, 0.86, that.torus);
 
-    //shape 4 and light
+    //shape 4
     that.polyhedra = createPolyhedra(that.scene, 1, 1, 5, 2, 0);
+    that.polyhedra = createSphere(that.scene, 1, 1, 5,);
 
-    //shape 5 and light
+    //shape 5 
     that.polyhedra = createPolyhedra(that.scene, 12, 1, 10, 2, 0);
     that.light = createAnyLight(that.scene, 2, 10, 5, 0, 0.24, 0.24, 0.91, that.polyhedra);
 
-    //walls
     that.box = createBox(that.scene, -13, 2.5, 0, 0.5, 5, 9, false); //wall 
     that.box = createBox(that.scene, -8, 2.5, 0, 0.5, 5, 9, false); //wall
     that.box = createBox(that.scene, -3, 2.5, 0, 0.5, 5, 9, false); //wall 
@@ -210,9 +205,10 @@ import {
     that.box = createBox(that.scene, 7, 2.5, 0, 0.5, 5, 9, false); //wall 
     that.box = createBox(that.scene, 12, 2.5, 0, 0.5, 5, 9, false); //wall 
 
-    //ground and main camera
-    that.ground = createGround(that.scene, 25, 10, -0.5);
+
+    that.ground = createGround(that.scene);
     that.camera = createArcRotateCamera(that.scene);
     return that;
   }
-  //----------------------------------------------------
+  //-------------------------------------------------------
+  //-------------------------------------------------------
